99f30eb55b0cb886947c134dd4c3c0dd20773349
laibasarwar <sarwar.laiba@gmail.com>
2022-10-04 09:06:52 -0400

    add npm axios

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { WEATHER_APP_API_KEY } from "../API_KEYS";
+
+function Home() {
+  return (
+    <div>
+      <h1>Weather App</h1>
+      <p>[WEATHER_APP_API_KEY]</p>
+    </div>
+  );
+}
+
+export default Home;
